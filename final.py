f2dc064c76960c7e5e3314e73407c0213e4ea72f
I-mdimple <141025672+I-mdimple@users.noreply.github.com>
2025-04-23 14:18:43 +0530

    Update final.py

--- final.py
+++ final.py
@@ -86,7 +86,7 @@ def turn_into_story(summary):
 
 # Streamlit UI
 st.title("📺 Dimple - YouTube/Uploaded Video Summarizer Using Groq LLM")
-st.image("PragyanAI_Transperent.png")
+st.image("PIZZA.jpg")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
